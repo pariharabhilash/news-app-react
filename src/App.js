a55913607330fee7aaafe0d29bafa1d1f387ef5e
Abhilash <pariharabhilash22@gmail.com>
2023-01-17 21:06:29 +0530

    router fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import News from "./components/News";
 
